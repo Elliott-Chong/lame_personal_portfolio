5512ebcff597cc8cf77959a5643a17933ce4a85c
Elliott Chong <elliottchong16@gmail.com>
2022-06-19 19:02:35 +0800

    add rockets

--- javascript/career.js
+++
@@ -1,9 +0,0 @@
-import { qs } from "./utils.js";
-
-qs("tr", true).forEach((row) => {
-  row.onclick = (e) => {
-    let career = e.target.parentNode.children[1].innerText;
-    career = career.toLowerCase().split(" ").join("-");
-    window.location.href = `#${career}`;
-  };
-});
